6d2104c68cbbab485d6f4a1199f2a02910893b0e
Punit Jain <contactpunitjain@gmail.com>
2021-03-30 15:26:56 +0530

    playing with v-if and v-show

--- conditionalRender/app.js
+++ conditionalRender/app.js
@@ -1,7 +1,16 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goals: [],
+      inputGoal: ''
+    };
   },
+  methods: {
+    addGoal() {
+      this.goals.push(this.inputGoal)
+      console.log(this.goals)
+    }
+  }
 });
 
 app.mount('#user-goals');
