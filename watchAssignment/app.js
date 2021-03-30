be2d0053afe3b114e4b6d8ca4594a203df17098e
Punit Jain <contactpunitjain@gmail.com>
2021-03-30 09:59:51 +0530

    Assignment on watch and computed

---
+++ watchAssignment/app.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            result: 0,
+            // message: ''
+        }
+    },
+    computed: {
+        message() {
+            if (this.result < 37) {
+                return "Not there yet"
+            }
+            else if (this.result === 37) {
+                return this.result
+            }
+            else {
+                return "Too much!"
+            }
+        }
+    },
+    watch: {
+        result() {
+            setTimeout(() => {
+                this.result = 0
+            }, 5000)
+        }
+    },
+    methods: {
+        addBy5() {
+            this.result = this.result + 5
+        },
+        addBy1() {
+            this.result = this.result + 1
+        }
+    }
+})
+
+app.mount('#assignment')
