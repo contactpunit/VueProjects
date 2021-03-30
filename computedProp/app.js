a5ec9424d7ebcc76b8b5280e7942befb682ceb1d
Punit Jain <contactpunitjain@gmail.com>
2021-03-30 07:49:33 +0530

    Added computed as an object to vue configuration

---
+++ computedProp/app.js
@@ -0,0 +1,43 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      confirmName: ''
+    };
+  },
+  computed: {
+    fullName() {
+      if (this.name === '') {
+        return ''
+      }
+      else {
+        return this.name + ' ' + 'Jain '
+      }
+    }
+  },
+  methods: {
+    confirmedInput() {
+      this.confirmName = this.name
+    },
+    increment(num) {
+      this.counter = this.counter + num
+    },
+    decrement(num) {
+      if (this.counter > 0) {
+        this.counter = this.counter - num
+      }
+    },
+    readName(event) {
+      this.name = event.target.value
+    },
+    submitForm() {
+      // pass
+    },
+    clearInput() {
+      this.name = ''
+    }
+  }
+})
+
+app.mount('#events')
