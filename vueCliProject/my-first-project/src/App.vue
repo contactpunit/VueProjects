85fc2d6efdd3e3e9d0e36ec8309aeb1ce5f2d64a
Punit Jain <contactpunitjain@gmail.com>
2021-03-31 21:17:34 +0530

    added config object and linked it to app configuration

---
+++ vueCliProject/my-first-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'punit',
+                    name: 'Punit Jain',
+                    age: 40,
+                    phone: '112232534676',
+                    email: 'punit@example.com'
+                },
+                {
+                    id: 'rahul',
+                    name: 'Rahul Jain',
+                    age: 30,
+                    phone: '5453454366',
+                    email: 'rahul@example.com'
+                }]
+        }
+    }
+}
+</script>
