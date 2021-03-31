df81a3ba900bd01b8c9ff109895998491b1fc0a0
Punit Jain <contactpunitjain@gmail.com>
2021-03-31 21:38:13 +0530

    added new component

---
+++ vueCliProject/my-first-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+    <li>
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="this.visible">
+            <li><strong>Phone:</strong>{{friend.phone}}</li>
+            <li><strong>Email:</strong>{{friend.email}}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            visible: false,
+            friend: {
+                    id: 'punit',
+                    name: 'Punit Jain',
+                    age: 40,
+                    phone: '112232534676',
+                    email: 'punit@example.com'
+                }
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.visible = !this.visible
+        }
+    }
+}
+</script>
