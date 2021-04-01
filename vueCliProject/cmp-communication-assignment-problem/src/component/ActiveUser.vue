46b940901f2ad6e00974af6ec90c117d413b328a
Punit Jain <contactpunitjain@gmail.com>
2021-04-01 17:49:36 +0530

    vue component communication exercise

---
+++ vueCliProject/cmp-communication-assignment-problem/src/component/ActiveUser.vue
@@ -0,0 +1,25 @@
+<template>
+<li>
+    <h2>{{ name }}</h2>
+    <h3>{{ age }}</h3>
+</li>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: String,
+            required: true
+        }
+    },
+}
+</script>
---
+++ vueCliProject/cmp-communication-assignment-problem/src/component/UserData.vue
@@ -0,0 +1,31 @@
+<template>
+    <form @submit.prevent="submitUserData">
+        <div>
+            <label>Name</label>
+            <input type="text" v-model="name">
+        </div>
+        <div>
+            <label>Age</label>
+            <input type="text" v-model="age">
+        </div>
+        <button>Add User</button>
+    </form>
+</template>
+
+<script>
+export default {
+    emits: ['user-data'],
+    data() {
+        return {
+            id: '',
+            name: '',
+            age: ''
+        }
+    },
+    methods:{
+        submitUserData() {
+            this.$emit('user-data', this.name, this.age)
+        }
+    }
+}
+</script>
