088d60ee38c2e7f6f44e1cd2dc0596c4050f2428
Punit Jain <contactpunitjain@gmail.com>
2021-04-03 14:53:51 +0530

    added BaseDialog componnet which shows a custom dialog when error in form. This uses slots to fill the text passed to the component. Use the baseDialog in addResource component to show modal/dialog when error happens

--- PracticeComponents/src/components/learning-resources/AddResource.vue
+++ PracticeComponents/src/components/learning-resources/AddResource.vue
@@ -1,33 +1,57 @@
 <template>
-    <base-card>
-        <form>
-            <div class="form-control">
-                <label for="title">Title</label>
-                <input id="title" name="title" type="text" ref="titleInput" />
-            </div>
-            <div class="form-control">
-                <label for="description">Description</label>
-                <textarea name="description" id="description" ref="descriptionInput"></textarea>
-            </div>
-            <div class="form-control">
-                <label for="link">Link</label>
-                <input id="link" name="link" type="url" ref="linkInput"/>
-            </div>
-            <div>
-                <base-button type="submit" @click.prevent="readInputs">Add Resource</base-button>
-            </div>
-        </form>
-    </base-card>
+<base-dialog v-if="invalidInput === true" title="Invalid Input. Kindly Correct" @close="closeDialog">
+    <template #default>
+        <p>Unfortunately atleast one value is invalid</p>
+    </template>
+    <template #actions>
+        <base-button @click="closeDialog">Okay</base-button>
+    </template>
+</base-dialog>
+<base-card>
+    <form>
+        <div class="form-control">
+            <label for="title">Title</label>
+            <input id="title" name="title" type="text" ref="titleInput" />
+        </div>
+        <div class="form-control">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" ref="descriptionInput"></textarea>
+        </div>
+        <div class="form-control">
+            <label for="link">Link</label>
+            <input id="link" name="link" type="url" ref="linkInput"/>
+        </div>
+        <div>
+            <base-button type="submit"  @click.prevent="readInputs">Add Resource</base-button>
+        </div>
+    </form>
+</base-card>
+    
+
+    
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            invalidInput: false
+        }
+    },
     methods: {
         readInputs() {
             const titleInput = this.$refs.titleInput.value
             const descInput = this.$refs.descriptionInput.value
             const linkInput = this.$refs.linkInput.value
+            if (titleInput.trim() === '' || descInput.trim() === '' || linkInput.trim() === '') {
+                this.invalidInput = true
+                return
+            }
             this.addResource(titleInput, descInput, linkInput)
+        },
+
+        closeDialog() {
+            this.invalidInput = false
         }
     },
     inject: ['addResource']
