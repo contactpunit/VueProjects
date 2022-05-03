f74a8c51a7f0377a7077e46c928333fa78656924
Punit Jain <contactpunitjain@gmail.com>
2022-05-03 17:04:22 +0530

    added code for winning or loosing

--- MonsterSlayerReattempt/app.js
+++ MonsterSlayerReattempt/app.js
@@ -7,7 +7,8 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            specialAttackCounter: 0
+            specialAttackCounter: 0,
+            winner: null
         }
     },
     computed: {
@@ -15,25 +16,35 @@ const app = Vue.createApp({
             return this.specialAttackCounter % 3 !== 0
         }
     },
+    watch: {
+        playerHealth() {
+            if (this.playerHealth <= 0 && this.monsterHealth <= 0) this.winner = 'draw'
+            else if (this.playerHealth <= 0) this.winner = 'monster'
+        },
+        monsterHealth() {
+            if (this.playerHealth <= 0 && this.monsterHealth <= 0) this.winner = 'draw'
+            else if (this.monsterHealth <= 0) this.winner = 'player'
+        }
+    },
     methods: {
         attackMonster() {
             this.specialAttackCounter++
             this.monsterHealth -= calculateValue(5, 12)
-            this.attachPlayer()
+            this.attackPlayer()
         },
-        attachPlayer() {
+        attackPlayer() {
             this.playerHealth -= calculateValue(8, 18)
         },
         specialAttack() {
             this.specialAttackCounter++
             this.monsterHealth -= calculateValue(10, 20)
-            this.attachPlayer()
+            this.attackPlayer()
         },
         healPlayer() {
-            const healValue = calculateValue(12, 15)
+            const healValue = calculateValue(20, 30)
             if (this.playerHealth + healValue > 100) this.playerHealth = 100
             else this.playerHealth += healValue
-            this.attachPlayer()
+            this.attackPlayer()
         }
     }
 })
