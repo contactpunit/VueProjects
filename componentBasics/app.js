abff8ababe6c2c0deaf280bfc74914b00d7f2039
Punit Jain <contactpunitjain@gmail.com>
2022-05-04 17:02:35 +0530

    defined a new component

--- componentBasics/app.js
+++ componentBasics/app.js
@@ -1,21 +1,6 @@
 const app = Vue.createApp({
   data() {
-    return {
-      friends: [
-        {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "01234 5678 991",
-          email: "manuel@localhost.com",
-        },
-        {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "09876 543 221",
-          email: "julie@localhost.com",
-        },
-      ],
-    }
+    return {}
   },
 })
 
