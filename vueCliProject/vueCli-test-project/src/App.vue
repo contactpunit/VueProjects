fd8429655c9ff691a045f78bad5c32561ec92ef7
Punit Jain <contactpunitjain@gmail.com>
2022-05-21 14:52:56 +0530

    using vue cli created a project

---
+++ vueCliProject/vueCli-test-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<section>
+    <h2>My Friends Details</h2>
+    <ul>
+        <li></li>
+    </ul>
+</section>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'punit',
+                    name: 'Punit Jain',
+                    phone: '92445545454',
+                    email: 'punit@local.com'
+                },
+                {
+                    id: 'manu',
+                    name: 'Manu Jain',
+                    phone: '4546565656',
+                    email: 'manu@localhost'
+                }
+            ]
+        }
+    }
+}
+</script>
