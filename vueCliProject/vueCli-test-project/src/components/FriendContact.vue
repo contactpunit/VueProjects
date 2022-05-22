cade1c392ef09dfa959c8e8985dda788dbb74a3a
Punit Jain <contactpunitjain@gmail.com>
2022-05-22 15:28:15 +0530

    understanding Props

--- vueCliProject/vueCli-test-project/src/components/FriendContact.vue
+++ vueCliProject/vueCli-test-project/src/components/FriendContact.vue
@@ -1,31 +1,32 @@
 <template>
-    <li>
-        <h2>{{ friend.name }}</h2>
-        <button @click="toggleDetails">Show Details</button>
-        <ul v-if="detailsAreVisible">
-            <li><strong>Phone:</strong>{{ friend.phone }}</li>
-            <li><strong>Email:</strong>{{ friend.email }}</li>
-        </ul>
-    </li>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            detailsAreVisible: false,
-            friend: {
-                id: 'punit',
-                name: 'Punit Jain',
-                phone: '92445545454',
-                email: 'punit@local.com'
-            }
-        }
+  props: [],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "punit",
+        name: "Punit Jain",
+        phone: "92445545454",
+        email: "punit@local.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
     },
-    methods:{
-        toggleDetails() {
-            this.detailsAreVisible = !this.detailsAreVisible
-        }
-    }
-}
+  },
+};
 </script>
