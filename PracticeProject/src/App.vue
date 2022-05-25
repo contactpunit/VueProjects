ea628e09d0240ee32a85caf066cbc0a7d7751020
Punit Jain <contactpunitjain@gmail.com>
2022-05-25 20:02:34 +0530

    1. Passing hardcoded data to start with 2. Added component LearningResource which stores each element of an array. 3. Created a wrapper component with slot for card look

---
+++ PracticeProject/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <stored-resources :storedResources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+export default {
+  components: {
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The officialView.js doc',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn how to google',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
