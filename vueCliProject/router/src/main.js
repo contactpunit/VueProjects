e3a8e5df970930b2208d0a7b0f05965ae7b5f716
Punit Jain <contactpunitjain@gmail.com>
2022-05-29 19:47:19 +0530

    catchall as well as not found url

--- vueCliProject/router/src/main.js
+++ vueCliProject/router/src/main.js
@@ -5,13 +5,17 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import ErrorPage from './components/nav/ErrorPage.vue';
 
 const app = createApp(App);
 const router = createRouter({
   routes: [
+    { path: '/', redirect: '/teams' },
+    // { path: '/teams', component: TeamsList, alias: '/' },
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:catchAll(.*)', component: ErrorPage },
   ],
   history: createWebHistory(),
 });
