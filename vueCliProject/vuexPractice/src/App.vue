e9fce9a048779e12dfaaba9b69c9b7562cb25fc9
Punit Jain <contactpunitjain@gmail.com>
2022-05-31 14:21:55 +0530

    create a store and use it in a component

--- vueCliProject/vuexPractice/src/App.vue
+++ vueCliProject/vuexPractice/src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <button @click="increment">Add 1</button>
+    <!-- <h3>{{ counter }}</h3> -->
+    <!-- <button @click="increment" i>Add 1</button> -->
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+  },
+  methods: {
+    increment() {
+      this.$store.state.counter = this.$store.state.counter + 1;
+    },
   },
 };
 </script>
