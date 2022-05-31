3b1e258f3d5f5c3c8ae2b46eae83b4bfa1a719a2
Punit Jain <contactpunitjain@gmail.com>
2022-05-31 16:10:53 +0530

    passing getters as argument in other getter

--- vueCliProject/vuexPractice/src/main.js
+++ vueCliProject/vuexPractice/src/main.js
@@ -21,8 +21,8 @@ const store = createStore({
     finalCounter(state) {
       return state.counter * 5;
     },
-    normalizedCounter(state) {
-      const updated = state.counter * 2;
+    normalizedCounter(state, getters) {
+      const updated = getters.finalCounter;
       if (updated < 0) return 0;
       else if (updated > 100) return 100;
       else return updated;
