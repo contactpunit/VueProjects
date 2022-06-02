aa8656e169d71791006875ec68023b68fdb410fa
Punit Jain <contactpunitjain@gmail.com>
2022-06-02 15:14:45 +0530

    created component fies, added header page with router link and linked router components

--- CoachFinderProject/src/router.js
+++ CoachFinderProject/src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsList from './pages/requests/RequestsList.vue';
+import CatchAll from './pages/CatchAll.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: '/contact', component: null }],
+      component: CoachDetail,
+      children: [{ path: '/contact', component: ContactCoach }],
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:catchAll(.*)', component: null },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsList },
+    { path: '/:catchAll(.*)', component: CatchAll },
   ],
 });
 
