b6bf25b0246239fed90a44feb349d7b22abfad4b
Punit Jain <contactpunitjain@gmail.com>
2022-06-04 21:38:38 +0530

    regsiter a coach functionality

--- CoachFinderProject/src/pages/coaches/CoachRegistration.vue
+++ CoachFinderProject/src/pages/coaches/CoachRegistration.vue
@@ -2,17 +2,23 @@
   <section>
     <base-card>
       <h2>Register as a Coach</h2>
-      <coach-form></coach-form>
+      <coach-form @save-data="saveData"></coach-form>
     </base-card>
   </section>
 </template>
 
 <script>
-import CoachForm from './CoachForm.vue';
+import CoachForm from '../../components/ui/CoachForm.vue';
 
 export default {
   components: {
     CoachForm,
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/addCoach', data);
+      this.$router.replace('/coaches');
+    },
   },
 };
 </script>
--- CoachFinderProject/src/store/modules/coaches/actions.js
+++ CoachFinderProject/src/store/modules/coaches/actions.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  addCoach(context, payload) {
+    const data = {
+      firstName: payload.firstName,
+      lastName: payload.lastName,
+      description: payload.description,
+      areas: payload.areas,
+      hourlyRate: payload.rate,
+      id: 'demo',
+    };
+    context.commit('addCoach', data);
+  },
+};
