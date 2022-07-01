eb7697dc8f851e7dcfe5ba3d3815696c40a09d94
Punit Jain <contactpunitjain@gmail.com>
2022-07-01 19:17:49 +0530

    added components and dummy content to verfy router links work fine

---
+++ CoachApp/src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,5 @@
+<template>
+  Details for coach
+  <router-view></router-view>
+  <router-link to="/coaches/t1/contact">Contact Me</router-link>
+</template>
---
+++ CoachApp/src/pages/coaches/CoachesList.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register</router-link>
+    </div>
+    <ul>
+      List of coaches
+    </ul>
+  </section>
+</template>
