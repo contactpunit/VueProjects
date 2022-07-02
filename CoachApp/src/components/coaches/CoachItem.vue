b31362b0eb23000b1868f893059e1a5afa36cbd0
Punit Jain <contactpunitjain@gmail.com>
2022-07-02 20:31:47 +0530

    added coachlist component to render each coach

---
+++ CoachApp/src/components/coaches/CoachItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <li>
+    <h3>{{ fullname }}</h3>
+    <h4>${{ rate }}/hr</h4>
+    <div>
+      <span v-for="area in areas" :key="area"></span>
+    </div>
+    <div class="actions">
+      <router-link :to="contactLink">Contact</router-link>
+      <router-link :to="coachLink">View Details</router-link>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['firstName', 'lastName', 'rate', 'areas', 'id'],
+  computed: {
+    fullname() {
+      return this.firstName + ' ' + this.lastName;
+    },
+    contactLink() {
+      return '/coaches/' + this.id + '/contact';
+    },
+    coachLink() {
+      return '/coaches/' + this.id;
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
