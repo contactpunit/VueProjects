945b38ee8cf33b9e39c3fde1446732464a01c745
Punit Jain <contactpunitjain@gmail.com>
2022-07-04 12:50:18 +0530

    added coach detail page

--- CoachApp/src/pages/coaches/CoachDetail.vue
+++ CoachApp/src/pages/coaches/CoachDetail.vue
@@ -1,5 +1,62 @@
 <template>
-  Details for coach
-  <router-view></router-view>
-  <router-link to="/coaches/c1/contact">Contact Me</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>{{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested ? Reach out now!!</h2>
+        <base-button link :to="coachContactLink">Contact Me</base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  created() {
+    const reqCoach = this.$store.getters['coaches/coaches'].find(
+      (coach) => coach.id === this.id
+    );
+    this.selectedCoach = reqCoach;
+  },
+  computed: {
+    coachContactLink() {
+      return '/coaches/' + this.id + '/contact';
+    },
+    fullName() {
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+    description() {
+      return this.selectedCoach.description;
+    },
+    areas() {
+      return this.selectedCoach.areas;
+    },
+  },
+};
+</script>
