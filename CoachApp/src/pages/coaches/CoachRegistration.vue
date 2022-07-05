0c964f9da41546abe2e1b69e6aafca04661fd7ec
Punit Jain <contactpunitjain@gmail.com>
2022-07-05 15:11:18 +0530

    added coach registration form

--- CoachApp/src/pages/coaches/CoachRegistration.vue
+++ CoachApp/src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,17 @@
-<template>Register as a coach</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a Coach</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
