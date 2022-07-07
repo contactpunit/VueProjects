7b81f7bcb204cdbaa3b57cd329b219f8da240d60
Punit Jain <contactpunitjain@gmail.com>
2022-07-07 19:52:01 +0530

    1. added new component for loading spinner 2. use the spinner while loading data in CoachesList component

--- CoachApp/src/pages/coaches/CoachesList.vue
+++ CoachApp/src/pages/coaches/CoachesList.vue
@@ -6,9 +6,14 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-        <base-button link to="/register" v-if="!isCoach">Register</base-button>
+        <base-button link to="/register" v-if="!isCoach && !isLoading"
+          >Register</base-button
+        >
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -35,6 +40,7 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
       activeFilters: {
         backend: true,
         frontend: true,
@@ -62,15 +68,17 @@ export default {
       });
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
   },
   methods: {
     filterCoaches(filters) {
       this.activeFilters = filters;
     },
-    loadCoaches() {
-      this.$store.dispatch('coaches/loadCoaches');
+    async loadCoaches() {
+      this.isLoading = true;
+      await this.$store.dispatch('coaches/loadCoaches');
+      this.isLoading = false;
     },
   },
 };
