0d13ca10473a543a51161984d9c2435f027be058
Punit Jain <contactpunitjain@gmail.com>
2022-07-11 22:18:47 +0530

    added catchall page

---
+++ CoachApp/src/pages/CatchAll.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        Page couldnot be found. Checkout our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
