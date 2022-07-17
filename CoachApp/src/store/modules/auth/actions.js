93884aa9574c928f24eb741fb033b9236d606ad4
Punit Jain <contactpunitjain@gmail.com>
2022-07-17 14:03:39 +0530

    refactored code

--- CoachApp/src/store/modules/auth/actions.js
+++ CoachApp/src/store/modules/auth/actions.js
@@ -1,47 +1,15 @@
 export default {
   async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-    const resData = await response.json();
-    if (!response.ok) {
-      throw new Error(resData.message || 'Failed to authenticate');
-    }
-    context.commit('setUser', {
-      token: resData.idToken,
-      userId: resData.localId,
-      tokenExpiration: resData.expiresIn,
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
     });
   },
 
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-    const resData = await response.json();
-    if (!response.ok) {
-      throw new Error(resData.message || 'Failed to authenticate');
-    }
-    context.commit('setUser', {
-      token: resData.idToken,
-      userId: resData.localId,
-      tokenExpiration: resData.expiresIn,
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
     });
   },
 
@@ -50,6 +18,35 @@ export default {
       token: null,
       userId: null,
       tokenExpiration: null,
+    });
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url;
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=API_KEY';
+    } else {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=API_KEY';
+    }
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
+    const resData = await response.json();
+    if (!response.ok) {
+      throw new Error(resData.message || 'Failed to authenticate');
+    }
+    context.commit('setUser', {
+      token: resData.idToken,
+      userId: resData.localId,
+      tokenExpiration: resData.expiresIn,
     });
   },
 };
