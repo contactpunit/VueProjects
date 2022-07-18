1cc7ad5e9568b3edbd9fccd6ffca7dab3192716a
Punit Jain <contactpunitjain@gmail.com>
2022-07-18 15:19:06 +0530

    using refs

--- compoApiAssignment/src/App.vue
+++ compoApiAssignment/src/App.vue
@@ -2,7 +2,7 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3 v-if="showGoal">{{ goal }}</h3>
+  <h3 v-if="courseData.goalVisibility">{{ goal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
   <button @click="toggleGoal">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
@@ -14,18 +14,35 @@
 
 <script>
 // import { reactive, ref } from '@vue/reactivity';
+import { ref } from '@vue/reactivity';
 export default {
-  data() {
+  setup() {
+    const courseData = ref({
+      myGoal: 'Learn Vue Composition API',
+      goalVisibility: true,
+    });
+
+    function toggleGoal() {
+      courseData.value.goalVisibility = !courseData.value.goalVisibility;
+    }
+
     return {
-      goal: 'Learn Vue Composition API',
-      showGoal: true,
+      goal: courseData.value.myGoal,
+      courseData,
+      toggleGoal,
     };
   },
-  methods: {
-    toggleGoal() {
-      this.showGoal = !this.showGoal;
-    },
-  },
+  // data() {
+  //   return {
+  //     goal: 'Learn Vue Composition API',
+  //     showGoal: true,
+  //   };
+  // },
+  // methods: {
+  //   toggleGoal() {
+  //     this.showGoal = !this.showGoal;
+  //   },
+  // },
   // setup() {
   //   const goal = ref('Learn Vue Composition API');
   //   const myGoal = reactive({
