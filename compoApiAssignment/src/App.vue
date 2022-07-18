e14dd9567b4afebf7cb9fd07ffd1b41c1b2b25a3
Punit Jain <contactpunitjain@gmail.com>
2022-07-18 14:55:47 +0530

    using reactive and ref

--- compoApiAssignment/src/App.vue
+++ compoApiAssignment/src/App.vue
@@ -2,9 +2,9 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="myGoal.showGoal">{{ goal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="toggleGoal">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,9 +13,22 @@
 </template>
 
 <script>
+import { reactive, ref } from '@vue/reactivity';
 export default {
-  
-}
+  setup() {
+    const goal = ref('Learn Vue Composition API');
+    const myGoal = reactive({
+      goal: 'Learn Vue Composition API',
+      showGoal: true,
+    });
+
+    function toggleGoal() {
+      myGoal.showGoal = !myGoal.showGoal;
+    }
+
+    return { goal, myGoal, toggleGoal };
+  },
+};
 </script>
 
 <style>
