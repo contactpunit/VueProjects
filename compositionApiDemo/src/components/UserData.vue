572cd4e8ff27e408b04c94e30df082c6ac1db70a
Punit Jain <contactpunitjain@gmail.com>
2022-07-18 21:23:08 +0530

    lifecycle hooks in composition api

--- compositionApiDemo/src/components/UserData.vue
+++ compositionApiDemo/src/components/UserData.vue
@@ -4,10 +4,29 @@
 </template>
 
 <script>
-import { computed, inject } from 'vue';
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+} from 'vue';
 export default {
   props: ['firstName', 'lastName'],
   setup(props) {
+    onBeforeMount(function () {
+      console.log('on before mount called');
+    });
+    onMounted(function () {
+      console.log('mounted called');
+    });
+    onBeforeUpdate(function () {
+      console.log('before update');
+    });
+    onUpdated(function () {
+      console.log('on updated called');
+    });
     const userName = computed(function () {
       return props.firstName + ' ' + props.lastName;
     });
