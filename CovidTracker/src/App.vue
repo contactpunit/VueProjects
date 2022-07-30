cb23dda46381fdb56835d1cad82c5e714eb81c61
Punit Jain <contactpunitjain@gmail.com>
2022-07-30 22:56:56 +0530

    select country option

--- CovidTracker/src/App.vue
+++ CovidTracker/src/App.vue
@@ -1,3 +1,44 @@
+<template>
+  <header>
+    <h2>Covid-19 Tracker</h2>
+  </header>
+  <div>
+    <section class="topic">
+      <strong>Global</strong>
+      <div>
+        {{ currentDate }}
+      </div>
+    </section>
+  </div>
+  <section class="container cases">
+    <h3>Cases</h3>
+    <div><strong>New: </strong>{{ newCases }}</div>
+    <div><strong>Total: </strong>{{ totalCases }}</div>
+  </section>
+  <section class="container deaths">
+    <h3>Deaths</h3>
+    <div><strong>New: </strong>{{ newDeaths }}</div>
+    <div><strong>Total: </strong>{{ totalDeaths }}</div>
+  </section>
+  <div>
+    <select-country></select-country>
+  </div>
+</template>
+
+<script>
+import SelectCountry from './components/SelectCountry.vue';
+export default {
+  components: {
+    SelectCountry,
+  },
+  computed: {
+    currentDate() {
+      return new Date().toUTCString();
+    },
+  },
+};
+</script>
+
 <style>
 * {
   box-sizing: border-box;
@@ -5,6 +46,21 @@
 
 html {
   font-family: sans-serif;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 0.5rem;
+  background-color: #1415b4;
+  color: white;
+  text-align: center;
+  margin-bottom: 2rem;
+}
+
+.topic {
+  color: black;
+  text-align: center;
+  font: 500;
 }
 
 h2 {
@@ -20,6 +76,10 @@ h2 {
   font-weight: bold;
 }
 
+div {
+  margin: 0.5rem 0;
+}
+
 body {
   margin: 0;
   text-align: center;
@@ -32,5 +92,13 @@ body {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
   text-align: center;
+}
+
+.cases {
+  background-color: #d5d5ea;
+}
+
+.deaths {
+  background-color: #878799;
 }
 </style>
---
+++ CovidTracker/src/components/SelectCountry.vue
@@ -0,0 +1,37 @@
+<template>
+  <select v-model="selected" @change="optionSelected">
+    <option value="0">Select Country</option>
+    <option
+      v-for="country in allCountries"
+      :key="country.Slug"
+      :value="country.ISO2"
+    >
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: 0,
+      allCountries: [],
+    };
+  },
+  //   async created() {
+  //     const response = await fetch('https://api.covid19api.com/countries', {
+  //       method: 'GET',
+  //     });
+  //     const data = await response.json();
+  //     if (response.ok) {
+  //       this.allCountries = data;
+  //     }
+  //   },
+  methods: {
+    optionSelected() {
+      console.log(this.selected);
+    },
+  },
+};
+</script>
