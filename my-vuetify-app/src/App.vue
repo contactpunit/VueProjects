b1a9e23c0971841fcebb39102ce444e6aba0ddca
Punit Jain <contactpunitjain@gmail.com>
2022-08-04 16:15:45 +0530

    basic layout with drawer wireframe

--- my-vuetify-app/src/App.vue
+++ my-vuetify-app/src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main></v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-}
+  data: () => ({ drawer: null }),
+};
 </script>
