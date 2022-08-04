bd46954455c772220e8b8a01fba96d53f798bb38
Punit Jain <contactpunitjain@gmail.com>
2022-08-04 16:37:19 +0530

    added navigation drawer

--- my-vuetify-app/src/App.vue
+++ my-vuetify-app/src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app>
-      <!--  -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">Vuetify Todo</v-list-item-title>
+          <v-list-item-subtitle> manage todo's! </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -16,6 +35,14 @@
 
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: "Todo", icon: "mdi-view-dashboard" },
+        { title: "About", icon: "mdi-help-box" },
+      ],
+    };
+  },
 };
 </script>
