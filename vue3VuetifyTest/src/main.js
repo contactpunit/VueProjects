6183d04f4a884fe9b68b9815c0300218689dc97b
Punit Jain <contactpunitjain@gmail.com>
2022-08-06 14:00:17 +0530

    removed unrequired files

--- vue3VuetifyTest/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue"
-import App from "./App.vue"
-import vuetify from "./plugins/vuetify"
-import { loadFonts } from "./plugins/webfontloader"
-import router from "./router/index.js"
-
-loadFonts()
-
-createApp(App).use(vuetify).use(router).mount("#app")
