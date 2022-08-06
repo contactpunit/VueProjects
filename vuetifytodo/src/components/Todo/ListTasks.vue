54382fde0ca87d002885f9290cdf7576a9a7fd31
Punit Jain <contactpunitjain@gmail.com>
2022-08-06 17:40:18 +0530

    moved per task item to a separate component

--- vuetifytodo/src/components/Todo/ListTasks.vue
+++ vuetifytodo/src/components/Todo/ListTasks.vue
@@ -1,47 +1,18 @@
 <template>
   <v-list flat class="pt-0">
-    <div v-for="task in allTasks" :key="task.id">
-      <v-list-item
-        :class="{ 'blue lighten-4': task.done }"
-        @click="toggleDone(task.id)"
-      >
-        <template #default>
-          <v-list-item-action>
-            <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title
-              :class="{ 'text-decoration-line-through': task.done }"
-            >
-              {{ task.title }}
-            </v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-btn icon @click.stop="deleteTask(task.id)">
-              <v-icon color="primary">mdi-delete</v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </template>
-      </v-list-item>
-      <v-divider></v-divider>
-    </div>
+    <task-item v-for="task in allTasks" :key="task.id" :task="task"></task-item>
   </v-list>
 </template>
 
 <script>
+import TaskItem from "./TaskItem.vue";
 export default {
+  components: {
+    TaskItem,
+  },
   computed: {
     allTasks() {
       return this.$store.getters.getAllTasks;
-    },
-  },
-  methods: {
-    toggleDone(id) {
-      this.$store.dispatch("selectChange", id);
-    },
-    deleteTask(id) {
-      this.$store.dispatch("deleteTask", id);
     },
   },
 };
