22941f548d6fa5af8dba2ce442055ac7d31870cf
Punit Jain <contactpunitjain@gmail.com>
2022-08-07 13:29:24 +0530

    added dialog box to handle deletion of task

--- vuetifytodo/src/components/Todo/TaskItem.vue
+++ vuetifytodo/src/components/Todo/TaskItem.vue
@@ -17,14 +17,18 @@
           </v-list-item-title>
         </v-list-item-content>
         <v-list-item-action>
-          <v-btn icon @click.stop="deleteTask(task.id)">
+          <v-btn icon @click.stop="enableDialog(task.id)">
             <v-icon color="primary">mdi-delete</v-icon>
           </v-btn>
         </v-list-item-action>
       </template>
     </v-list-item>
     <v-divider></v-divider>
-    <base-dialog></base-dialog>
+    <base-dialog
+      :dialogProp="dialogProp"
+      v-if="dialogProp.dialogShow"
+      @cancel-dialog="dialogProp.dialogShow = false"
+    ></base-dialog>
   </div>
 </template>
 
@@ -32,16 +36,41 @@
 import BaseDialog from "../UI/BaseDialog.vue";
 
 export default {
+  data() {
+    return {
+      dialogProp: {
+        dialogTitle: "",
+        dialogText: "",
+        dialogShow: false,
+        dialogTaskId: "",
+      },
+    };
+  },
   components: {
     BaseDialog,
   },
+  provide() {
+    return {
+      deleteTask(id) {
+        this.$store.dispatch("deleteTask", id);
+      },
+    };
+  },
   props: ["task"],
+  computed: {
+    showDialog() {
+      return this.dialogProp.dialogShow;
+    },
+  },
   methods: {
     toggleDone(id) {
       this.$store.dispatch("selectChange", id);
     },
-    deleteTask(id) {
-      this.$store.dispatch("deleteTask", id);
+    enableDialog(id) {
+      this.dialogProp.dialogShow = true;
+      this.dialogProp.dialogTitle = "Delete Task";
+      this.dialogProp.dialogText = "Do you want to delete the task ?";
+      this.dialogProp.dialogTaskId = id;
     },
   },
 };
--- vuetifytodo/src/components/UI/BaseDialog.vue
+++ vuetifytodo/src/components/UI/BaseDialog.vue
@@ -1,20 +1,19 @@
 <template>
-  <v-dialog :value="true" persistent max-width="290">
+  <v-dialog :value="dialogProp.dialogShow" persistent max-width="290">
     <v-card>
       <v-card-title class="text-h5">
-        Use Google's location service?
+        {{ dialogProp.dialogTitle }}
       </v-card-title>
-      <v-card-text
-        >Let Google help apps determine location. This means sending anonymous
-        location data to Google, even when no apps are running.</v-card-text
-      >
+      <v-card-text>{{ dialogProp.dialogText }}</v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="green darken-1" text @click="dialog = false">
-          Disagree
-        </v-btn>
-        <v-btn color="green darken-1" text @click="dialog = false">
-          Agree
+        <v-btn color="red darken-1" text @click="cancelDialog"> No </v-btn>
+        <v-btn
+          color="green darken-1"
+          text
+          @click="deleteTask(dialogProp.dialogTaskId)"
+        >
+          Yes
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -22,7 +21,15 @@
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["dialogProp"],
+  inject: ["deleteTask"],
+  methods: {
+    cancelDialog() {
+      this.$emit("cancel-dialog");
+    },
+  },
+};
 </script>
 
 <style>
