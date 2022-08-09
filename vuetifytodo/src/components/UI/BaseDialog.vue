ab4b8dfaba29f50df72217bb8a9a98e607849c1d
Punit Jain <contactpunitjain@gmail.com>
2022-08-09 15:01:29 +0530

    added validation for keyup as well as save event

--- vuetifytodo/src/components/UI/BaseDialog.vue
+++ vuetifytodo/src/components/UI/BaseDialog.vue
@@ -29,7 +29,14 @@
         <v-spacer></v-spacer>
         <v-btn color="red darken-1" text @click="cancelDialog"> Cancel </v-btn>
 
-        <v-btn color="green darken-1" text @click="saveTask"> Save </v-btn>
+        <v-btn
+          color="green darken-1"
+          text
+          @click="saveTask"
+          :disabled="isNotValid"
+        >
+          Save
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -47,16 +54,26 @@ export default {
   mounted() {
     this.taskTitle = this.task.title;
   },
+  computed: {
+    isNotValid() {
+      if (!this.taskTitle) return true;
+      if (this.task.title === this.taskTitle) return true;
+      return false;
+    },
+  },
   methods: {
     cancelDialog() {
       this.$emit("cancel-dialog");
     },
     saveTask() {
-      this.$store.dispatch("editTitle", {
-        title: this.taskTitle,
-        id: this.task.id,
-      });
-      this.$emit("cancel-dialog");
+      console;
+      if (!this.isNotValid) {
+        this.$store.dispatch("editTitle", {
+          title: this.taskTitle,
+          id: this.task.id,
+        });
+        this.$emit("cancel-dialog");
+      }
     },
   },
 };
