273f0abc365b932a421528c13cc066dc5fcdc1f2
Punit Jain <contactpunitjain@gmail.com>
2022-08-13 16:38:13 +0530

    added TopNav component

--- storeui/src/components/TopNav.vue
+++ storeui/src/components/TopNav.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>TopNav</div>
+  <div>
+    <v-app-bar>
+      <v-toolbar-title>Awesome Shop</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <span class="hidden-sm-and-up">
+        <v-btn> Menu </v-btn>
+      </span>
+
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn to="/" text>
+          <v-icon small left>mdi-home</v-icon>
+          Home
+        </v-btn>
+        <v-btn to="/store" text>
+          <v-icon small left>mdi-store</v-icon>
+          Store
+        </v-btn>
+        <v-btn to="/cart" text>
+          <v-icon small left>mdi-cart</v-icon>
+          Cart
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
