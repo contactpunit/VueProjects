42031cdb7dad4a5419ae530549edbaf43a98f610
Punit Jain <contactpunitjain@gmail.com>
2022-08-13 16:53:30 +0530

    added drawer menu for small screen size, make sure router works

--- storeui/src/components/TopNav.vue
+++ storeui/src/components/TopNav.vue
@@ -5,7 +5,7 @@
 
       <v-spacer></v-spacer>
       <span class="hidden-sm-and-up">
-        <v-btn> Menu </v-btn>
+        <v-btn @click.stop="drawer = !drawer"> Menu </v-btn>
       </span>
 
       <v-toolbar-items class="hidden-xs-only">
@@ -23,6 +23,21 @@
         </v-btn>
       </v-toolbar-items>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary right>
+      <v-list>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.link">
+          <v-list-item-icon>
+            <v-icon small>mdi-{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
@@ -30,10 +45,11 @@
 export default {
   data() {
     return {
+      drawer: false,
       items: [
         { title: "Home", link: "/", icon: "home" },
-        { title: "Store", link: "/store", icon: "shopping-basket" },
-        { title: "Cart", link: "cart", icon: "shopping-cart" },
+        { title: "Store", link: "/store", icon: "store" },
+        { title: "Cart", link: "cart", icon: "cart" },
       ],
     };
   },
