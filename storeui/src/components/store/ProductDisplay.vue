c310aa23f95495d60eeb4bc45c5d224a50ea711c
Punit Jain <contactpunitjain@gmail.com>
2022-08-14 17:13:56 +0530

    completed product display in store

---
+++ storeui/src/components/store/ProductDisplay.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>Products</h3>
+    <v-row>
+      <v-col
+        sm="6"
+        md="4"
+        v-for="product in $store.state.products"
+        :key="product.name"
+      >
+        <vertical-card :product="product"></vertical-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import VerticalCard from "../cards/VerticalCard.vue";
+
+export default {
+  components: {
+    VerticalCard,
+  },
+};
+</script>
--- storeui/src/views/StoreList.vue
+++ storeui/src/views/StoreList.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>Store</div>
+  <v-container>
+    <v-row>
+      <v-col sm="3" offset-lg="1">
+        <sidebar-display></sidebar-display>
+      </v-col>
+      <v-col sm="9" lg="7">
+        <product-display></product-display>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import ProductDisplay from "@/components/store/ProductDisplay.vue";
+import SidebarDisplay from "@/components/store/SidebarDisplay.vue";
+
+export default {
+  components: {
+    ProductDisplay,
+    SidebarDisplay,
+  },
+};
+</script>
