b27e35d7596ced2ab4c5bca509c4c73d739baf96
Punit Jain <contactpunitjain@gmail.com>
2022-08-20 08:21:23 +0530

    fixed bug and passed params to method

--- compositionVue3/src/views/HomeView.vue
+++ compositionVue3/src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <template>
+  <h2>{{ appTitle }}</h2>
   <div class="home">
     <h3>{{ counterData.title }}</h3>
     <div>
-      <button class="btn" @click="decrement">-</button>
+      <button class="btn" @click="decrement(2)">-</button>
       <span class="counter">{{ counterData.count }}</span>
-      <button class="btn" @click="increment">+</button>
+      <button class="btn" @click="increment(5)">+</button>
     </div>
     <div class="edit">
       <h4>Edit counter title</h4>
@@ -16,21 +17,26 @@
 <script setup>
 import { reactive } from "vue";
 
+const appTitle = "My App";
 const counterData = reactive({
   count: 0,
   title: "CounterApp",
 });
 
-function increment() {
-  return counterData.count++;
+function increment(amount) {
+  counterData.count = counterData.count + amount;
 }
 
-function decrement() {
-  return counterData.count--;
+function decrement(amount) {
+  counterData.count -= amount;
 }
 </script>
 
 <style>
+h2 {
+  text-align: center;
+  padding: 20px;
+}
 .home {
   text-align: center;
   padding: 20px;
