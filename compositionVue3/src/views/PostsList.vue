b293af9dcd1e69371e8c2025059fd689b14593a2
Punit Jain <contactpunitjain@gmail.com>
2022-08-20 19:27:16 +0530

    added v-for to loop over posts

--- compositionVue3/src/views/PostsList.vue
+++ compositionVue3/src/views/PostsList.vue
@@ -2,15 +2,34 @@
     <div class="posts">
         <h2>All Posts</h2>
         <ul>
-            <li>
-                <router-link to="/post/1">Post 1</router-link>
-            </li>
-            <li>
-                <router-link to="/post/2">Post 2</router-link>
+            <li v-for="post in posts" :key="post.id">
+                <router-link :to="`/post/${post.id}`">{{post.title}}</router-link>
             </li>
         </ul>
     </div>
 </template>
+
+<script setup>
+
+import {ref} from 'vue'
+
+const posts = ref([
+    {
+        id: '1',
+        title: 'Post 1'
+    },
+    {
+        id: '2',
+        title: 'Post 2'
+    },
+    {
+        id: '3',
+        title: 'Post 3'
+    }
+])
+
+</script>
+
 
 <style scoped>
 ul {
