996719f68cdc535acda49d6e6554bd726d0a8ff4
Punit Jain <contactpunitjain@gmail.com>
2022-08-21 09:44:59 +0530

    child components and lazy loading in composition api

---
+++ compositionVue3/src/components/UserModal.vue
@@ -0,0 +1,22 @@
+<template>
+<teleport to="body">
+    <div class="modal">
+            <h1>This is a modal</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur consequatur distinctio nulla temporibus a tempore expedita repellat exercitationem dolor, ipsum, aliquid, sunt excepturi? Ad quasi, error fugiat facilis corrupti explicabo?</p>
+            <button @click="hideModal">Hide Modal</button>
+        </div>
+</teleport>
+</template>
+
+<style scoped>
+.modal {
+    background: beige;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    z-index: 1;
+}
+</style>
--- compositionVue3/src/router/index.js
+++ compositionVue3/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ModalView from '../views/ModalView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,7 +23,7 @@ const router = createRouter({
     {
       path: '/modal',
       name: 'modal',
-      component: () => import('../views/ModalView.vue')
+      component: ModalView
     }
   ]
 })
