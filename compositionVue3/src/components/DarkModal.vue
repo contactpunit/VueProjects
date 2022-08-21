68a1db3ee1b445f241f96e0be5ae74d14aa2eef2
Punit Jain <contactpunitjain@gmail.com>
2022-08-21 20:35:00 +0530

    dynamic componnets

---
+++ compositionVue3/src/components/DarkModal.vue
@@ -0,0 +1,32 @@
+<template>
+<teleport to="body">
+    <div class="modal-dark" v-if="modelValue">
+            <h1>{{ props.title }}</h1>
+            <slot></slot>
+            <button @click="$emit('update:modelValue', false)">Hide Modal</button>
+        </div>
+</teleport>
+</template>
+
+<script setup>
+
+const props = defineProps(['modelValue', 'title'])
+const modalEvent = defineEmits(['update:modelValue'])
+
+
+</script>
+
+
+<style scoped>
+.modal-dark {
+    background: #333;
+    color: white;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    z-index: 1;
+}
+</style>
--- compositionVue3/src/views/ModalView.vue
+++ compositionVue3/src/views/ModalView.vue
@@ -1,19 +1,26 @@
 <template>
     <div class="modal">
         <h1>Modal</h1>
+        <div>
+            <label>Show dark modals? </label>
+            <input type="checkbox" v-model="isDarkModal">
+        </div>
         <button @click="showModal" v-if="!modalOn">Show Modal</button>
-        <user-modal v-model="modalOn" title="This is a Modal">
+        <component v-model="modalOn" title="This is a Modal" :is="isDarkModal ? darkModal : UserModal">
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur consequatur distinctio nulla temporibus a tempore expedita repellat exercitationem dolor, ipsum, aliquid, sunt excepturi? Ad quasi, error fugiat facilis corrupti explicabo?</p>
-        </user-modal>
+        </component>
+        
     </div>
 </template>
 
 <script setup>
 import UserModal from '../components/UserModal.vue'
+import darkModal from '../components/DarkModal.vue'
 
 import {ref} from 'vue'
 
 const modalOn = ref(false)
+const isDarkModal = ref(false)
 
 const showModal = () => {
     modalOn.value = true
