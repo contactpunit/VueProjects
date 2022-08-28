88d3fd87780e3f2165dce4d20911f32678f63d1d
Punit Jain <contactpunitjain@gmail.com>
2022-08-28 17:55:57 +0530

    manually added vue-router

--- noteballs/src/main.js
+++ noteballs/src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: []
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
