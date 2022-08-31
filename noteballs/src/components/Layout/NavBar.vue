65a35f8719fe4cf0b128a165c169cde9d8faac74
Punit Jain <contactpunitjain@gmail.com>
2022-08-31 19:59:38 +0530

    style navbar-menu

--- noteballs/src/components/Layout/NavBar.vue
+++ noteballs/src/components/Layout/NavBar.vue
@@ -1,26 +1,50 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <div class="navbar-item" is-size-4 is-family-monospace>Noteballs</div>
+    <div class="container is-max-desktop">
+      <div class="navbar-brand">
+        <div class="navbar-item" is-size-4 is-family-monospace>Noteballs</div>
 
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
+        <a
+          role="button"
+          class="navbar-burger"
+          :class="{ 'is-active': showNav }"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          @click.prevent="showNav = !showNav"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': showNav }"
       >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-end">
-        <router-link class="navbar-item" to="/" active-class="is-active"> Notes </router-link>
-        <router-link class="navbar-item" to="/stats" active-class="is-active"> Stats </router-link>
+        <div class="navbar-end">
+          <router-link class="navbar-item" to="/"> Notes </router-link>
+          <router-link class="navbar-item" to="/stats"> Stats </router-link>
+        </div>
       </div>
     </div>
   </nav>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const showNav = ref(false);
+</script>
+
+<style scoped>
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%
+  }
+}
+</style>
