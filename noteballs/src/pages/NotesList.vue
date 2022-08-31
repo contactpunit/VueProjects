bc5d3372abda78d4d102cb6b296d252f26318c66
Punit Jain <contactpunitjain@gmail.com>
2022-08-31 20:44:31 +0530

    add new note form

--- noteballs/src/pages/NotesList.vue
+++ noteballs/src/pages/NotesList.vue
@@ -1,5 +1,32 @@
 <template>
-    <div class="notes">
-        <h1>Notes</h1>
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="add a new note"></textarea>
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">Add New Note</button>
+        </div>
+      </div>
     </div>
+
+    <div class="card">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          sed harum iste deserunt quas cum ab unde quidem minus, minima, sint
+          perspiciatis maiores repudiandae? Inventore labore tenetur asperiores
+          eveniet a.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
 </template>
