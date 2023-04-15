123a50fb580d0ba64e2cd7cd04e56cd883ff7622
Punit Jain <contactpunitjain@gmail.com>
2023-04-15 16:16:43 +0530

    added computed property

--- CompositionApp/compositionApp/src/views/HomeView.vue
+++ CompositionApp/compositionApp/src/views/HomeView.vue
@@ -8,19 +8,27 @@
     </div>
 
     <div class="edit">
+      <p>This counter is {{ oddOrEven }}</p>
       <h4>Edit counter title: </h4>
       <input v-model="counterData.counterTitleValue" type="text">
     </div>
+
+
   </div>
 </template>
 
 
 <script setup>
-import { reactive, ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
 
 const counterData = reactive({
   counterValue: 10,
   counterTitleValue: 'My Test App'
+})
+
+const oddOrEven = computed(() => {
+  if(counterData.counterValue % 2 === 0) return 'even'
+  else return 'odd'
 })
 
 const counter = ref( 0 )
@@ -62,5 +70,9 @@ h3 {
 
 .edit {
   margin-top: 60px;
+}
+
+.edit p {
+  margin-bottom: 30px;
 }
 </style>
