180c4461c6e70c9098d2cfd99d624f3d71551af2
Punit Jain <contactpunitjain@gmail.com>
2023-04-16 18:19:49 +0530

    added v-AutoFocus directive to new component

--- CompositionApp/compositionApp/src/views/AboutView.vue
+++ CompositionApp/compositionApp/src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is About Page</h1>
+    <textarea name="about" id="about" cols="2" rows="2" placeholder="Add About text" v-AutoFocus></textarea>
   </div>
 </template>
+
+<script setup>
+import {vAutoFocus} from '@/directives/vAutoFocus'
+</script>
+
+<style scoped>
+textarea {
+  width: 30rem;
+  height: 10rem
+}
+</style>
