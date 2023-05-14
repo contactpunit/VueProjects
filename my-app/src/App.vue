f357a61a830d44578c1ad87e49be9d3fa96ee5d6
Punit Jain <contactpunitjain@gmail.com>
2023-05-14 12:27:33 +0530

    added Timeline component with Bulma css

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+import Timeline from "./components/Timeline.vue";
 </script>
 
 <template>
+  <div class="section">
+    <div class="container">
+      <timeline></timeline>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
 </style>
---
+++ my-app/src/components/Timeline.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a href="#">Today</a>
+      <a href="#">This Week</a>
+      <a href="#">This Month</a>
+    </span>
+  </nav>
+</template>
