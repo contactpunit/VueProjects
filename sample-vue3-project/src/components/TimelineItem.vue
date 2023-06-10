edbbc060ea5dcd0dc9f2dba8ded66d4bc4213186
Punit Jain <contactpunitjain@gmail.com>
2023-06-10 16:49:27 +0530

    vue reactivity using ref

--- sample-vue3-project/src/components/TimelineItem.vue
+++ sample-vue3-project/src/components/TimelineItem.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 const periods = ['Today', 'This Week', 'This Month']
+const selectedPeriod = ref('')
+const selectPeriod = (period: string) => {
+  selectedPeriod.value = period
+}
 </script>
 
 <template>
   <nav class="is-primary panel">
+    {{ selectedPeriod }}
     <span class="panel-tabs">
-      <a :class="period" v-for="period of periods" :key="period">{{ period }}</a>
+      <a
+        :class="{ 'is-active': period === selectedPeriod }"
+        v-for="period of periods"
+        :key="period"
+        @click="selectPeriod(period)"
+        >{{ period }}</a
+      >
     </span>
   </nav>
 </template>
