1c09f777a53272800da1f527b04abf95668a5b09
Punit Jain <contactpunitjain@gmail.com>
2023-12-17 13:17:22 +0530

    type script with vue. Refs, reactive and computed

---
+++ vueTypeScript/src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import TheHeader from "@/components/TheHeader.vue";
+import EntryEditor from "./components/EntryEditor.vue";
+import EntryCard from "@/components/EntryCard.vue";
+import { reactive } from "vue";
+import type User from "./types/User"
+
+const user: User = reactive({
+  id: 1,
+  username: 'Punit',
+  userSettings: []
+})
+
+console.log(user)
+
+
+</script>
+
+<template>
+  <main class="container m-auto p-10">
+    <TheHeader />
+    <EntryEditor />
+    <ul>
+      <li>
+        <EntryCard />
+      </li>
+    </ul>
+  </main>
+</template>
---
+++ vueTypeScript/src/components/EntryEditor.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import EmojiField from "@/components/EmojiField.vue";
+import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import { ref, computed } from "vue"
+import type Emoji from "../types/Emoji"
+
+const text = ref("")
+
+const emoji = ref<Emoji|null>(null)
+
+const charCount = computed(()=> text.value.length)
+
+const handleTextLength = (e: Event) => {
+  const textData = e.target as HTMLTextAreaElement
+  if(textData.value.length > 280) textData.value = text.value = textData.value.substring(0, 280)
+}
+
+</script>
+<template>
+  <form class="entry-form" @submit.prevent>
+    <textarea :value="text" @keyup="handleTextLength" placeholder="New Journal Entry for danielkelly_io"></textarea>
+    <EmojiField v-model="emoji"/>
+    <div class="entry-form-footer">
+      <span>{{ charCount }} / 280</span>
+      <button>Remember <ArrowCircleRight width="20" /></button>
+    </div>
+  </form>
+</template>
