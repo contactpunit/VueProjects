3009ed78833abfd3d5134cb29d5cf60197cace51
Punit Jain <contactpunitjain@gmail.com>
2023-12-19 13:47:49 +0530

    props in components

--- coffeeApp/plan-picker/app.js
+++ coffeeApp/plan-picker/app.js
@@ -1,5 +1,17 @@
-Vue.createApp({})
+Vue.createApp({
+    data() {
+        return {
+            plans: ['The Addict','The Curious', 'The Single', 'The Hilarious']
+        }
+    }
+})
 .component('plan', {
-    template: '#plan-template'
+    template: '#plan-template',
+    props: {
+        name: {
+            type: String,
+            required: true
+        }
+    }
 })
 .mount('#app')
