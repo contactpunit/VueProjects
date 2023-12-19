1c7c59af9eeb3b8767b3b8d39d807e654246d9a4
Punit Jain <contactpunitjain@gmail.com>
2023-12-19 14:04:59 +0530

    local components

--- coffeeApp/plan-picker/app.js
+++ coffeeApp/plan-picker/app.js
@@ -1,20 +1,28 @@
-Vue.createApp({
-})
-.component('planpicker', {
-    template: '#planpicker-template',
-    data() {
-        return {
-            plans: ['The Addict','The Curious', 'The Single', 'The Hilarious']
-        }
-    }
-})
-.component('plan', {
+let planComponent = {
     template: '#plan-template',
     props: {
         name: {
             type: String,
             required: true
         }
+    }
+}
+
+let planPickerComponent = {
+    template: '#planpicker-template',
+    components: {
+        plan: planComponent
+    },
+    data() {
+        return {
+            plans: ['The Addict','The Curious', 'The Single', 'The Hilarious']
+        }
+    }
+}
+
+Vue.createApp({
+    components: {
+        planpicker: planPickerComponent
     }
 })
 .mount('#app')
