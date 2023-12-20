31572a02137f8d24fe782675ae15c1cf9c7bb7cb
Punit Jain <contactpunitjain@gmail.com>
2023-12-20 12:57:01 +0530

    added click event on close button

--- notificationMsg/app.js
+++ notificationMsg/app.js
@@ -2,7 +2,18 @@ Vue.createApp({}).
 component('notification-message', {
     template: '#notification-message-template',
     props: {
-        type: String
+        type: String,
+        default: 'info'
+    },
+    data(){
+        return {
+            hidden: false
+        }
+    },
+    methods: {
+        hideNotification() {
+            this.hidden = true
+        }
     }
 })
 .mount('#app')
