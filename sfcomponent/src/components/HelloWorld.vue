5c191ed5f78d34ecb695e9229515bdea70e4bd91
Punit Jain <contactpunitjain@gmail.com>
2023-12-20 19:47:42 +0530

    registering and displaying a component

---
+++ sfcomponent/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <display-username username="contactpunit"></display-username>
+  </div>
+</template>
+
+<script>
+import DisplayUsername from '@/components/DisplayUsername.vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    DisplayUsername
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
