d41abb673883e95cde875eaef463931832eb9ce9
Punit Jain <contactpunitjain@gmail.com>
2023-12-23 17:27:55 +0530

    added dynamic router-link urls

--- vueTravel/src/views/HomePage.vue
+++ vueTravel/src/views/HomePage.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="home">
-        <h1>Home</h1>
+        <h1>Destinations</h1>
+        <div class="destinations">
+            <router-link v-for="destination in destinations" :key="destination.id" :to="destination.slug">
+                <h2>{{ destination.name }}</h2>
+                <img :src="`/images/${destination.image}`" :alt="destination.name">
+            </router-link>
+        </div>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import sourceData from '../data.json'
+import { reactive } from 'vue'
+
+const destinations = reactive(sourceData.destinations)
+
+</script>
