3530ea43e4e2e739825a804b59581ba5fe62cbd5
Punit Jain <contactpunitjain@gmail.com>
2024-01-06 19:01:38 +0530

    adding menu items

--- vuetify-layout/src/pages/ApplicationLayout.vue
+++ vuetify-layout/src/pages/ApplicationLayout.vue
@@ -2,12 +2,27 @@
   <v-layout class="rounded rounded-md">
     <v-app-bar title="Application bar" :color="`rgb(0, 0, 256)`">
       <template #append>
+
+      <v-menu>
+      <template v-slot:activator="{ props }">
         <v-avatar
         size="42"
         rounded="0"
         color="blue"
+        v-bind="props"
         >
       <span>PJ</span></v-avatar>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in ['Profile', 'Settings', 'Logout']"
+          :key="index"
+          :value="index"
+        >
+          <v-list-item-title>{{ item }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
       </template>
     </v-app-bar>
 
