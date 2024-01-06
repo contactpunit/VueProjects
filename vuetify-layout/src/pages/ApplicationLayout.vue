37629e4a7fbdf4e0d27e5196b1e6a0cf5235d352
Punit Jain <contactpunitjain@gmail.com>
2024-01-06 18:13:45 +0530

    adding an avatar in app bar

--- vuetify-layout/src/pages/ApplicationLayout.vue
+++ vuetify-layout/src/pages/ApplicationLayout.vue
@@ -1,6 +1,15 @@
 <template>
   <v-layout class="rounded rounded-md">
-    <v-app-bar title="Application bar" :color="`rgb(0, 0, 256)`"></v-app-bar>
+    <v-app-bar title="Application bar" :color="`rgb(0, 0, 256)`">
+      <template #append>
+        <v-avatar
+        size="42"
+        rounded="0"
+        color="blue"
+        >
+      <span>PJ</span></v-avatar>
+      </template>
+    </v-app-bar>
 
     <v-navigation-drawer>
       <v-list>
