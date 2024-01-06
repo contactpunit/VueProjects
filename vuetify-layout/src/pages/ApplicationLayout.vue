6a231dbeaa3581e5f54bb31832c13f7401df95f9
Punit Jain <contactpunitjain@gmail.com>
2024-01-06 22:02:49 +0530

    v-navigation-drawer and creating router links for the items

--- vuetify-layout/src/pages/ApplicationLayout.vue
+++ vuetify-layout/src/pages/ApplicationLayout.vue
@@ -1,8 +1,18 @@
 <template>
   <v-layout class="rounded rounded-md">
-    <v-app-bar title="Application bar" :color="`rgb(0, 0, 256)`">
+    <v-app-bar 
+    :color="`rgb(0, 0, 256)`"
+    >
+      <template #prepend>
+        <v-app-bar-nav-icon variant="text" @click="($event)=> drawer = !drawer">
+        </v-app-bar-nav-icon>
+        <v-toolbar-title>
+          <router-link to="/"
+          class="text-decoration-none text-white"
+          >My App</router-link>
+        </v-toolbar-title>
+      </template>
       <template #append>
-
       <v-menu>
       <template v-slot:activator="{ props }">
         <v-avatar
@@ -26,10 +36,18 @@
       </template>
     </v-app-bar>
 
-    <v-navigation-drawer>
-      <v-list>
+    <v-navigation-drawer
+    expand-on-hover
+        rail
+        v-model="drawer"
+    >
+      <!-- <v-list>
         <v-list-item title="Navigation drawer"></v-list-item>
-      </v-list>
+      </v-list> -->
+      <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-note" title="Posts" :to="`/posts`"></v-list-item>
+          <v-list-item prepend-icon="mdi-comment" title="Comments" :to="`/comments`"></v-list-item>
+        </v-list>
     </v-navigation-drawer>
 
     <v-main class="d-flex align-center justify-center" style="min-height: 600px;">
@@ -37,3 +55,9 @@
     </v-main>
   </v-layout>
 </template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+
+const drawer = ref(false)
+</script>
